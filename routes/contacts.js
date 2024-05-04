4e98e2d8957a4cb69c319577be1ee4f266aa4d9f
Taylor Christensen <kuleman47@byui.edu>
2024-05-03 22:11:25 -0400

    Week 2 finished

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,6 +2,8 @@ const routes = require("express").Router();
 
 const contactsController = require("../controllers/contacts");
 
-routes.get("/", contactsController.getData);
+routes.get("/", contactsController.getAllData);
+
+routes.get("/:id", contactsController.getSingleData);
 
 module.exports = routes;
